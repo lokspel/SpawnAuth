67af8f2f319a783e67af46b1df509db3dac1752f
lokspel <208148594+lokspel@users.noreply.github.com>
2026-03-31 09:30:27 +0400

    refactor: remove redundant limbo protection events

--- src/main/java/me/lokspel/spawnauth/events/OnLimboProtectionEvent.java
+++ src/main/java/me/lokspel/spawnauth/events/OnLimboProtectionEvent.java
@@ -1,19 +1,8 @@
 package me.lokspel.spawnauth.events;
 
 import me.lokspel.spawnauth.helpers.GameHelper;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.event.weather.ThunderChangeEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
@@ -22,76 +11,6 @@ public class OnLimboProtectionEvent implements Listener {
 
     public OnLimboProtectionEvent(GameHelper gameHelper) {
         this.gameHelper = gameHelper;
-    }
-
-    @EventHandler
-    private void onBlockBreak(BlockBreakEvent event) {
-        if (gameHelper.isInAuthWorld(event.getPlayer().getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onBlockPlace(BlockPlaceEvent event) {
-        if (gameHelper.isInAuthWorld(event.getPlayer().getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onPlayerInteract(PlayerInteractEvent event) {
-        if (gameHelper.isInAuthWorld(event.getPlayer().getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-        if (gameHelper.isInAuthWorld(event.getPlayer().getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
-        if (gameHelper.isInAuthWorld(event.getPlayer().getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onInventoryClick(InventoryClickEvent event) {
-        if (event.getWhoClicked() instanceof Player player && gameHelper.isInAuthWorld(player.getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onDrop(PlayerDropItemEvent event) {
-        if (gameHelper.isInAuthWorld(event.getPlayer().getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onPickup(EntityPickupItemEvent event) {
-        if (event.getEntity() instanceof Player player && gameHelper.isInAuthWorld(player.getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onSwapHand(PlayerSwapHandItemsEvent event) {
-        if (gameHelper.isInAuthWorld(event.getPlayer().getLocation())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player player && gameHelper.isInAuthWorld(player.getLocation())) {
-            event.setCancelled(true);
-        }
     }
 
     @EventHandler
